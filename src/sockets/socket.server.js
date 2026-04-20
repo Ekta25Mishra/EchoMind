ab987ea294afc066363923e442f186a7a488c2d0
Ekta25Mishra <mishrae3104@gmail.com>
2026-04-20 19:57:42 +0530

    Started a project

---
+++ src/sockets/socket.server.js
@@ -0,0 +1,34 @@
+const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+
+function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
+
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      next(new Error("Authentication error: No token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = decoded;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error: Invalid token"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+    //...
+    console.log("New socket connection:", socket.id);
+  });
+}
+
+module.exports = initSocketServer;
