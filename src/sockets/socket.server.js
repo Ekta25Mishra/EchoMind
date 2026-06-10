bf575d120d56380ad61ee80a8885f327ef3ea1e7
Ekta25Mishra <mishrae3104@gmail.com>
2026-06-10 17:11:02 +0530

    Limit out

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -49,7 +49,9 @@ function initSocketServer(httpServer) {
       const memory = await queryMemory({
         queryVector: vectors,
         limit:3,
-        metadata:{}
+        metadata:{
+          user:socket.user.id
+        }
       })
 
       await createMemory({
@@ -61,23 +63,34 @@ function initSocketServer(httpServer) {
           text: messagePayload.content
         }
       })
-
-      
-      console.log(memory);
       
       
       const chatHistory = (await messageModel.find({
         chat: messagePayload.chat,
       }).sort({createdAt: -1}).limit(20).lean()).reverse()
 
-      const response = await aiService.generateResponse(
-        chatHistory.map((item) => {
+      const stm = chatHistory.map((item) => {
           return {
             role: item.role,
             parts: [{ text: item.content }],
           };
-        }),
-      );
+        })
+
+        const ltm = [
+          {
+            role:"user",
+            parts: [ { text: `
+              these are some previous messages from the chat, use them to generate a response
+              ${memory.map(item => item.metadata.text).join("\n")}
+              `}]
+          }
+        ]
+
+       console.log(ltm[0]);
+       console.log(stm);
+       
+        
+      const response = await aiService.generateResponse([...ltm,...stm]);
 
        const responseMessage = await messageModel.create({
         chat: messagePayload.chat,
